f050ee0086ceca61231ac6f9ee073dbba21a676e
Hadad <hadad@linuxmail.org>
2025-02-23 13:58:46 +0700

    jarvis: Initial.

---
+++ app/src/main/java/dev/workers/hadadrjt/ai/MainActivity.java
@@ -0,0 +1,25 @@
+package dev.workers.hadadrjt.ai;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://ai.hadadrjt.workers.dev/");
+    }
+}
