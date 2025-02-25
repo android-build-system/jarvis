3f2f49391300106b9704cb909c48e1b64a14da18
Hadad <hadad@linuxmail.org>
2025-02-25 15:28:01 +0700

    jarvis: Automatic detect screen orientation mode.
    - Potrait.
    - Landscape.

--- app/src/main/java/dev/workers/hadadrjt/ai/MainActivity.java
+++ app/src/main/java/dev/workers/hadadrjt/ai/MainActivity.java
@@ -1,22 +1,30 @@
 package dev.workers.hadadrjt.ai;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView webView = findViewById(R.id.webView);
+        webView = findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://ai.hadadrjt.workers.dev/");
+        webView.loadUrl("https://ai.hadadrjt.workers.dev");
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
     }
 }
